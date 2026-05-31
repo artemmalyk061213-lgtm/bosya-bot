e4aee7801b168f045798c6e686ad1e64b6a3663e
artemmalyk061213-lgtm <artemmalyk061213@gmail.com>
2026-05-31 08:40:19 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,9 @@ def start_message(message):
     markup.add(btn1, btn2)
     markup.add(btn3)
     markup.add(btn4, btn5)
-    with open('1780200644977.png', 'rb') as photo:
+    with open('1780200649477.png', 'rb') as photo:
         bot.send_photo(message.chat.id, photo, caption="👋 Добро пожаловать в бота!", reply_markup=markup)
+
 
 
 
